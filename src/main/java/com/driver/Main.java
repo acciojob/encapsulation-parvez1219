72a096fb39a3e90eca14c8acd8aa097c8c502cd5
parvez1219 <parvezrahaman1219@gmail.com>
2023-02-11 15:01:09 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly b=new RWOnly();
+        // name has private access in com.driver.RWOnly
+        b.setName("naveen");
+        System.out.print(b.getName());
+    }
 }
